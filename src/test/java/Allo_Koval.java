a464088382717db435f5b59db708e37473ff5ff8
Teftelia007 <oleksii00712@gmail.com>
2021-04-26 02:36:53 +0300

    1 Try 26/04/2021

---
+++ src/test/java/Allo_Koval.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Allo_Koval {
+    private WebDriver driver;
+    /*BeforeAll
+    public static void profileSetUp(){
+        System.setProperties("webdriver.chrome.driver","src\\main\\resources\\chromedriver.exe");
+    }*/
+    @BeforeEach
+    public void testsSetup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.youtube.com/watch?v=OKWl5brw8TE");
+    }
+
+    @Test
+    public void Teardown() throws InterruptedException{
+
+    }
+    @AfterEach
+    public void tearDown() {driver.close();}
+}
